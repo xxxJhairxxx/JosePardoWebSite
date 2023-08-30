202e77ccfe731e6f7837999d6a415ea56a4582f2
C:/Program Files/Git/jhair <jinfanzonquispe30@gmail.com>
2023-08-29 23:07:31 -0500

    plugin populate-deep

--- admin/config/plugins.js
+++ admin/config/plugins.js
@@ -1,21 +1,38 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
+  // Otras configuraciones aquí...
+
+  'users-permissions': {
+    config: {
       jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  },
+  'strapi-plugin-populate-deep': {
+    config: {
+      defaultDepth: 10, // Default is 5
+    },
+  },
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: "/api/",
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
       },
     },
-  });
+  },
+});
