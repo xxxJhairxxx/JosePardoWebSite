a344fabd78628432ee19082158f87b76c80f5e1e
C:/Program Files/Git/jhair <jinfanzonquispe30@gmail.com>
2024-01-06 01:05:21 -0500

    add changes

--- admin/config/database.js
+++ admin/config/database.js
@@ -55,7 +55,7 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', true) && {
+        ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
           ca: env('DATABASE_SSL_CA', undefined),
